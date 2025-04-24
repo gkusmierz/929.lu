bd1018f3157c1d2e7ec77d075d0c5fde5bec2d72
gkusmierz <gk@protone.io>
2025-04-24 16:56:04 +0200

    Revert "Initial implementation of audio playback"
    This reverts commit 9c78671d21ca68c35314137ec10bd0bf4e1a13c1.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,9 @@
-import {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    IonButton,
-    IonToggle,
-    IonLabel,
-    IonItem,
-} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import  audioService from '../services/AudioService';
-import { useState, useEffect } from 'react';
 
 const Home: React.FC = () => {
-    const [isPlaying, setIsPlaying] = useState<boolean>(false);
-    const [useAAC, setUseAAC] = useState<boolean>(true);
-    const audioService = new AudioService();
-
-        const subscription = audioService.isPlaying$.subscribe(isPlaying => {
-            setIsPlaying(isPlaying);
-        });
-        return () => subscription.unsubscribe();
-    }, []);
-
-    const togglePlayback = () => {
-        const streamUrl = useAAC ? 'https://radio.929.lu/mobile-aacp' : 'https://radio.929.lu/mobile-mp3';
-        audioService.togglePlayPause(streamUrl);
-    };
-
-    const toggleStream = (event: CustomEvent) => {
-        setUseAAC(event.detail.checked);
-        audioService.changeStream(event.detail.checked);
-    };
-    return (
+  return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
@@ -47,18 +16,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-            <ExploreContainer name="Home page" />
-        <IonButton expand="block" onClick={togglePlayback}>
-            {isPlaying ? 'Stop' : 'Play'}
-        </IonButton>
-        <IonItem>
-            <IonLabel>AAC</IonLabel>
-            <IonToggle
-                checked={useAAC}
-                onIonChange={toggleStream}
-            />
-            <IonLabel>MP3</IonLabel>
-        </IonItem>
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
